055c22ef0879b04b27a800c0b6d1be51c56659d0
E-Merrill <emerril3@uwyo.edu>
2025-04-12 12:05:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+function depthFirstSearch(graph, startNode, targetNode, path, visited = new Set()) {
+    if (startNode == targetNode){
+        path.push(startNode);
+        return true;
+    }
+    visited.add(startNode);
+    for (let j = 0; j < graph[startNode].length; j++){
+        if(graph[startNode][j]){
+            if(visited.has(j)){
+                continue;
+            }
+            if(depthFirstSearch(graph, j, targetNode, path, visited)) {
+                path.push(startNode);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function search(graph, startNode, targetNode){
+    if(graph.length == 0){
+        return [];
+    }
+    let path = [];
+    if(depthFirstSearch(graph, startNode, targetNode, path)){
+        
+        //reversed because pushing things to an array puts it at the end, and we add the path
+        //as we 'go back' from finished recursive calls
+
+        path.reverse();
+        return path;
+    }
+    else{return []};
 }
